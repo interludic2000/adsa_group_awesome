548a9266b5ab027b2072e3c0b4f70edb2b3f8579
interludic2000 <xiangguo@internode.on.net>
2014-08-17 01:21:55 -0700

    Updated poly java. Should pass websubmission

--- PolyMultiply.java
+++ PolyMultiply.java
@@ -1,159 +1,133 @@
-import java.util.Vector;
+/*
+ * ADSA Assignment 2, question 3
+ * 
+ * a1077337 Xiang Guo
+ * a1646630 Patrick Man
+ * 
+ */
 
+import java.rmi.RemoteException;
 
-public class PolyMultiply {
+public class PolyMulti {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-
-		/*
-		 * make vector 1,2,3
-		 */
-		Vector<Integer> p = new Vector<Integer>();
-		p.add(1);
-		p.add(2);
-		p.add(3);
-		
-		Vector<Integer> q = new Vector<Integer>();
-		q.add(4);
-		q.add(5);
-		q.add(6);
-		
-		Vector<Integer> r = new Vector<Integer>(p.size() * 2);	
-		
-		r = PolyMulti(p,q);
-		
-		for(int i = 0; i< r.size(); i++){
-			System.out.println("r(" + i + ") = " + r.get(i));
-		}
-	}
-
 	
-	public static Vector<Integer> PolyMulti(Vector<Integer> p, Vector<Integer> q){
-		System.out.println("--- Function Called ---");
-		for(int i = 0; i < p.size(); i++){
-			System.out.println("p[" +i + "] = " + p.get(i));
-		}
-		for(int i = 0; i < q.size(); i++){
-			System.out.println("q[" +i + "] = " + q.get(i));
-		}
-		
-		assert(p.size() == q.size());
-		
-		
+	public static int[] polyMulti(int[] p, int[] q)throws RemoteException{
+		int n = p.length;
+		int m = q.length;
+				
 		/*
 		 * Base case
 		 */
-		if (p.size() <= 1 && q.size() <=1){
-			Vector<Integer> r = new Vector<Integer>();;
-			r.add(multiply(p.get(0),q.get(0)));
+
+		if (n <= 1 && m <=1){
+			int[] r = new int[1];
+			r[0] = multiply(p[0],q[0]);
 			return r;
 		}
-		
 
-		int p1_len= p.size()/2;
-		Vector<Integer> p1 = new Vector<Integer>(p1_len);
-		Vector<Integer> p2 = new Vector<Integer>(p.size() - p1_len);
-
-		
-		if(p.size() > 1){
-			/*
-			 * Split p into p1 and p2
-			 */
-			int k = p.size()/2;
-
+		/*
+		 * Split p into p1 and p2
+		 */
+		int p1_len= n/2;
+		int[] p1 = new int[p1_len];
+		int[] p2 = new int[n - p1_len];
+			
+		if(n > 1){
+			int k = n/2;
 			for (int i = 0; i < k; i++) 
-				p1.add(p.get(i));
+				p1[i] = p[i];
 			
-			for (int i = 0; i < (p.size()-k); i++) 
-				p2.add(p.get(k+i));	
+			for (int i = 0; i < (n-k); i++) 
+				p2[i] = p[k+i];	
 		}
 	
+		/*
+		 * Split q into q1 and q2
+		 */
+		int q1_len = m/2;
+		int[] q1 = new int[q1_len];
+		int[] q2 = new int[m - q1_len];
 
-		int q1_len= q.size()/2;
-		Vector<Integer> q1 = new Vector<Integer>(q1_len);
-		Vector<Integer> q2 = new Vector<Integer>(q.size() - q1_len);
-		
-		if(q.size() > 1){
-			/*
-			 * Split p into p1 and p2
-			 */
-			int l = q.size()/2;
+		if(m > 1){
+			int l = m/2;
 			for (int i = 0; i < l; i++) 
-				q1.add(q.get(i));
+				q1[i] = q[i];
 			
-			for (int i = 0; i < (q.size()-l); i++) 
-				q2.add(q.get(l+i));	
+			for (int i = 0; i < (m-l); i++) 
+				q2[i] = q[l+i];
 		}
-	
+
+		
 		/*
 		 * Recursive call
 		 */
-		Vector<Integer> A = PolyMulti(p1,q1);
-		Vector<Integer> C = PolyMulti(p2,q2);
+		int A[] = polyMulti(p1,q1);
+		int C[] = polyMulti(p2,q2);
 		
-
 		/*
 		 * Calculate (p1(x) + p2(x))
 		 */
-		Vector<Integer> p3 = new Vector<Integer>(p2.size());
-		for(int i = 0; i < p2.size(); i++){
-			System.out.println("p2.size() = " + p2.size());
-			if(p1.size() < p2.size() && p2.size() > 1 ){
-				p1.add(0);
-			}
-	
-			p3.add(p1.get(i) + p2.get(i));
+		int[] p3 = new int[max(p1.length,p2.length)];
 
-			System.out.println("p3[" + i + "] = " + p3.get(i));
+		for(int i = 0; i < q1.length; i++){
+			p3[i] = p1[i] + p2[i];
+			if(p1.length<p2.length){
+				p3[i+1] = 0 + p2[i+1];
+			}
 		}
 
 		/*
 		 * Calculate (q1(x) + q2(x))
 		 */
-		Vector<Integer> q3 = new Vector<Integer>(q2.size());
-		for(int i = 0; i < q2.size(); i++){
-			System.out.println("q2.size() = " + q2.size());
-			if(q1.size() < q2.size() && q2.size() > 1 ){
-				q1.add(0);
-			}
-			q3.add(q1.get(i) + q2.get(i));
-			System.out.println("q3[" + i + "] = " + q3.get(i));
+		int[] q3 = new int[max(q1.length,q2.length)];
+		
+		for(int i = 0; i < q1.length; i++){
+			q3[i] = q1[i] + q2[i];
+			if(q1.length<q2.length)
+				q3[i+1] = 0 + q2[i+1];
 		}
 		
 		/*
 		 * Call B(x) = ((p1(x) + p2(x)) x ((q1(x) + q2(x)) - A(x) - C(x)
+		 * 
 		 */
+		int B[] = new int[p1.length+p2.length];
+		B =	polyMulti(p3,q3);
 		
-		Vector<Integer> B = new Vector<Integer>(p1.size() + p2.size());
-		B =	PolyMulti(p3,q3);
+		for( int i = 0; i < A.length ; i++){
+			B[i] = B[i] - A[i];	
+		}
 		
-		B.set(0, B.get(0) - A.get(0) - C.get(0));
-
+		for( int i = 0; i < C.length ; i++){
+			B[i] = B[i] - C[i];	
+		}
+		
 		
 		/*
 		 * Put A B and C into array r 
+		 * 
 		 */
+		int[] r = new int[n*2-1];
 		
-		Vector<Integer> r = new Vector<Integer>();
-		for(int i = 0; i < A.size(); i++){
-			r.add(A.get(i));
+		for(int i = 0; i < A.length; i++){
+			r[i] = r[i] + A[i];
 		}
-		for(int i = 0; i < B.size(); i++){
-			r.add(B.get(i));
+
+		for(int i = 0; i < B.length; i++){
+			r[i+n/2] = r[i+n/2] + B[i];
 		}
 		
-		for(int i = 0; i < C.size(); i++){
-			r.add(C.get(i));
+		for(int i = 0; i < C.length; i++){
+			r[i+(n/2)*2] = r[i+(n/2)*2] + C[i];
 		}
 		
 		return r;
 	}
 	
 	public static int multiply(int a, int b){
-		System.out.println(a + " x " + b);
 		int c = a * b;
 		return c;
 	}
