32852f1d9062338b0dfce89e9ec051832a66af0d
interludic2000 <xiangguo@internode.on.net>
2014-08-16 17:05:01 -0700

    redone in vector

--- PolyMultiply.java
+++ PolyMultiply.java
@@ -1,3 +1,5 @@
+import java.util.Vector;
+
 
 public class PolyMultiply {
 
@@ -5,126 +7,146 @@ public class PolyMultiply {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		/*
-		 * Answer should be 4, 14, 28, 27, 18 
+		 * make vector 1,2,3
 		 */
-		int[] p = {1, 2, 3};
-		int[] q = {4, 5, 6};
-		int[] r = new int[p.length*2];
+		Vector<Integer> p = new Vector<Integer>();
+		p.add(1);
+		p.add(2);
+		p.add(3);
+		
+		Vector<Integer> q = new Vector<Integer>();
+		q.add(4);
+		q.add(5);
+		q.add(6);
+		
+		Vector<Integer> r = new Vector<Integer>(p.size() * 2);	
 		
 		r = PolyMulti(p,q);
-		for(int i = 0; i < r.length; i++){
-			System.out.println("r[" +i + "] = " + r[i]);
+		
+		for(int i = 0; i< r.size(); i++){
+			System.out.println("r(" + i + ") = " + r.get(i));
 		}
-		
-		
 	}
 
 	
-	public static int[] PolyMulti(int[] p, int[] q){
+	public static Vector<Integer> PolyMulti(Vector<Integer> p, Vector<Integer> q){
 		System.out.println("--- Function Called ---");
-		for(int i = 0; i < p.length; i++){
-			System.out.println("p[" +i + "] = " + p[i]);
+		for(int i = 0; i < p.size(); i++){
+			System.out.println("p[" +i + "] = " + p.get(i));
 		}
-		for(int i = 0; i < q.length; i++){
-			System.out.println("q[" +i + "] = " + q[i]);
+		for(int i = 0; i < q.size(); i++){
+			System.out.println("q[" +i + "] = " + q.get(i));
 		}
 		
-		assert(p.length == q.length);
+		assert(p.size() == q.size());
 		
 		
 		/*
 		 * Base case
 		 */
-		if (p.length <= 1 && q.length <=1){
-			int[] r = new int[p.length];
-			r[0] = multiply(p[0],q[0]);
+		if (p.size() <= 1 && q.size() <=1){
+			Vector<Integer> r = new Vector<Integer>();;
+			r.add(multiply(p.get(0),q.get(0)));
 			return r;
 		}
 		
-	
-		int p1_len= p.length/2;
-		int[] p1 = new int[p1_len];
-		int[] p2 = new int[p.length - p1_len];
-			
-		if(p.length > 1){
+
+		int p1_len= p.size()/2;
+		Vector<Integer> p1 = new Vector<Integer>(p1_len);
+		Vector<Integer> p2 = new Vector<Integer>(p.size() - p1_len);
+
+		
+		if(p.size() > 1){
 			/*
 			 * Split p into p1 and p2
 			 */
-			int k = p.length/2;
+			int k = p.size()/2;
+
 			for (int i = 0; i < k; i++) 
-				p1[i] = p[i];
+				p1.add(p.get(i));
 			
-			for (int i = 0; i < (p.length-k); i++) 
-				p2[i] = p[k+i];	
+			for (int i = 0; i < (p.size()-k); i++) 
+				p2.add(p.get(k+i));	
+		}
+	
+
+		int q1_len= q.size()/2;
+		Vector<Integer> q1 = new Vector<Integer>(q1_len);
+		Vector<Integer> q2 = new Vector<Integer>(q.size() - q1_len);
+		
+		if(q.size() > 1){
+			/*
+			 * Split p into p1 and p2
+			 */
+			int l = q.size()/2;
+			for (int i = 0; i < l; i++) 
+				q1.add(q.get(i));
+			
+			for (int i = 0; i < (q.size()-l); i++) 
+				q2.add(q.get(l+i));	
 		}
 	
 		/*
-		 * Split q into q1 and q2
-		 */
-		int q1_len = q.length/2;
-		int[] q1 = new int[q1_len];
-		int[] q2 = new int[q.length - q1_len];
-		
-		if(q.length > 1){
-
-			int l = q.length/2;
-			for (int i = 0; i < l; i++) 
-				q1[i] = q[i];
-			
-			for (int i = 0; i < (q.length-l); i++) 
-				q2[i] = q[l+i];
-		}
-		
-		/*
 		 * Recursive call
 		 */
-		int A[] = PolyMulti(p1,q1);
-		int C[] = PolyMulti(p2,q2);
+		Vector<Integer> A = PolyMulti(p1,q1);
+		Vector<Integer> C = PolyMulti(p2,q2);
 		
+
 		/*
 		 * Calculate (p1(x) + p2(x))
 		 */
-		int[] p3 = new int[p1.length];
-		for(int i = 0; i < p1.length; i++){
-			p3[i] = p1[i] + p2[i];
-			System.out.println("p3[" + i + "] = " + p3[i]);
+		Vector<Integer> p3 = new Vector<Integer>(p2.size());
+		for(int i = 0; i < p2.size(); i++){
+			System.out.println("p2.size() = " + p2.size());
+			if(p1.size() < p2.size() && p2.size() > 1 ){
+				p1.add(0);
+			}
+	
+			p3.add(p1.get(i) + p2.get(i));
+
+			System.out.println("p3[" + i + "] = " + p3.get(i));
 		}
-		
+
 		/*
 		 * Calculate (q1(x) + q2(x))
 		 */
-		int[] q3 = new int[q1.length];
-		for(int i = 0; i < q1.length; i++){
-			q3[i] = q1[i] + q2[i];
-			System.out.println("q3[" + i + "] = " + q3[i]);
+		Vector<Integer> q3 = new Vector<Integer>(q2.size());
+		for(int i = 0; i < q2.size(); i++){
+			System.out.println("q2.size() = " + q2.size());
+			if(q1.size() < q2.size() && q2.size() > 1 ){
+				q1.add(0);
+			}
+			q3.add(q1.get(i) + q2.get(i));
+			System.out.println("q3[" + i + "] = " + q3.get(i));
 		}
 		
 		/*
 		 * Call B(x) = ((p1(x) + p2(x)) x ((q1(x) + q2(x)) - A(x) - C(x)
-		 * I think the issue in here, maybe.
 		 */
-		int B[] = new int[p1.length+p2.length]; 
+		
+		Vector<Integer> B = new Vector<Integer>(p1.size() + p2.size());
 		B =	PolyMulti(p3,q3);
-		B[0] = B[0] - A[0] - C[0];
+		
+		B.set(0, B.get(0) - A.get(0) - C.get(0));
 
 		
 		/*
 		 * Put A B and C into array r 
-		 * 
 		 */
-		int[] r = new int[A.length + B.length + C.length];
-		for(int i = 0; i < A.length; i++){
-			r[i] = A[i];
+		
+		Vector<Integer> r = new Vector<Integer>();
+		for(int i = 0; i < A.size(); i++){
+			r.add(A.get(i));
 		}
-
-		for(int i = 0; i < B.length; i++){
-			r[i+A.length] = B[i];
+		for(int i = 0; i < B.size(); i++){
+			r.add(B.get(i));
 		}
 		
-		for(int i = 0; i < C.length; i++){
-			r[i+A.length+B.length] = C[i];
+		for(int i = 0; i < C.size(); i++){
+			r.add(C.get(i));
 		}
 		
 		return r;
